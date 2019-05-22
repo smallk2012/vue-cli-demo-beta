e7e288b2013ce7072a4e41640f725d16583d77be
cC <>
2019-05-22 11:22:45 +0800

    修复 console.log.apply(this, __arguments)  bug

--- src/api/extend/log.js
+++ src/api/extend/log.js
@@ -1,12 +1,12 @@
 import api from '@/api'
 /**
  * 统一打印调用方法
- * @param {需要打印的参数} __arguments
+ * @param {需要打印的参数} _arg
  */
-const log = (...__arguments) => {
+const log = (...arg) => {
     if (api.debug) {
         // eslint-disable-next-line
-        console.log.apply(this, __arguments)
+        console.log.apply(console, arg)
     }
 }
 export default log
